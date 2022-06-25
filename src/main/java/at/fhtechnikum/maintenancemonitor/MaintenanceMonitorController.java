922a75bcdbe0425d879677342ea650d72c1c7b67
Robert <wi21b059@technikum-wien.at>
2022-06-25 23:11:59 +0200

    delete controller

--- src/main/java/at/fhtechnikum/maintenancemonitor/MaintenanceMonitorController.java
+++
@@ -1,7 +0,0 @@
-package at.fhtechnikum.maintenancemonitor;
-
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.GetMapping;
-
-
