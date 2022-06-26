35cec041b9ee8acff7c2deadbccbf4f1c31576ec
Robert <wi21b059@technikum-wien.at>
2022-06-26 16:21:01 +0200

    changed structure of routes

--- src/main/java/at/fhtechnikum/maintenancemonitor/MaintenanceMonitorController.java
+++ src/main/java/at/fhtechnikum/maintenancemonitor/MaintenanceMonitorController.java
@@ -4,25 +4,30 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+
 import org.springframework.web.servlet.ModelAndView;
 
 @RestController
 class MaintenanceMonitorController {
 
     private String message;
-/*
-    @RequestMapping("/")
-    @ResponseBody
-    public String Index() {
-        return "Index";
+
+    @GetMapping("/api/maintenanceMode")
+    public String getMessage() {
+        return message;
     }
-*/
+
+    @PutMapping("/updateMessage/{message}")
+    public void updateMessage(@PathVariable String message) {
+        this.message = message;
+    }
+
+    @PutMapping("/resetMessage")
+    public void resetMessage() {
+        this.message = "-";
+    }
+
 public class MyRestController {
     @RequestMapping("/")
     public ModelAndView welcome() {
@@ -31,18 +36,5 @@ public class MyRestController {
         return modelAndView;
     }
 }
-    @GetMapping("/api/maintenanceMode")
-    public String getMessage(){
-        return message;
-    }
 
-    @PutMapping("/updateMessage/{message}")
-    public void updateMessage(@PathVariable String message){
-        this.message = message;
-    }
-
-    @PutMapping("/resetMessage")
-    public void resetMessage() {
-        this.message = "-";
-    }
 }
